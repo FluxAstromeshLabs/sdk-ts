598ef5df0084ffd3d4175c29676b6f06f3f559d0
5708646fe8691f1cf581b133aa0b806ac8de5396 <5708646fe8691f1cf581b133aa0b806ac8de5396>
2024-10-03 14:41:52 +0700

    chore: update phantom wallet error

--- packages/wallet/wallets/Phantom.ts
+++ packages/wallet/wallets/Phantom.ts
@@ -1,15 +1,24 @@
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
 import { ChainId } from '../../utils'
-
+const ERROR = {
+  WalletNotReadyError: 'Please install Phantom wallet'
+}
 export default class Phantom {
   private chainId: string
   constructor(args: { chainId: ChainId }) {
     this.chainId = args.chainId
   }
   async connect() {
-    const provider = new PhantomWalletAdapter()
-    await provider.connect()
-    return provider
+    try {
+      const provider = new PhantomWalletAdapter()
+      await provider.connect()
+      return provider
+    } catch (e) {
+      if (ERROR[e.name]) {
+        throw new Error(ERROR[e.name])
+      }
+      throw new Error(e.name)
+    }
   }
   async getAddresses(): Promise<Array<string>> {
     const provider = await this.connect()
@@ -20,9 +29,8 @@ export default class Phantom {
     const provider = await this.connect()
     return provider.publicKey?.toString() || ''
   }
-  async signPersonal(message: Uint8Array): Promise<Uint8Array> {
+  async signPersonal(message: any): Promise<Uint8Array> {
     const provider = await this.connect()
-
     if (provider.signMessage) {
       const signedMessage = await provider.signMessage(message)
       return signedMessage
