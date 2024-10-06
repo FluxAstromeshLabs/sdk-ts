cf38bfb9febd8ba653c148af3e2c471079e665e1
5708646fe8691f1cf581b133aa0b806ac8de5396 <5708646fe8691f1cf581b133aa0b806ac8de5396>
2024-10-06 14:01:09 +0700

    chore: update css

--- packages/ui/components/JsonViewer/index.vue
+++ packages/ui/components/JsonViewer/index.vue
@@ -1,49 +1,92 @@
 <template>
-  <div class="w-full overflow-x-auto transition-all duration-300">
-    <div v-for="(value, key) in data" :key="key" class="w-fit ml-4">
-      <div class="flex">
-        <div
-          v-if="isArray(value) || isObject(value)"
-          @click="toggle(key)"
-          class="arrow-icon"
-          :class="{
-            active: collapsed[key]
-          }"
-        >
-          <BaseIcons name="AngleDown" />
-        </div>
-        <span v-if="!isArray(data)" @click="toggle(key)" class="cursor-pointer">{{ key }}:</span>
-        <span v-if="isObject(value)">
-          <span v-if="collapsed[key]">{...}</span>
-          <span v-else>{</span>
-        </span>
-        <span v-else-if="isArray(value)">
-          <span v-if="collapsed[key]">[...]</span>
-          <span v-else>[</span>
-        </span>
-        <span v-else>{{ value }}</span>
+  <div
+    class="w-full overflow-x-auto transition-all duration-300"
+    :class="{
+      'ml-4': !isChildren
+    }"
+  >
+    <div class="flex items-center">
+      <div
+        v-if="!isChildren && (isArray(data) || isObject(data))"
+        @click="toggleRoot"
+        class="arrow-icon"
+        :class="{
+          active: collapsedRoot
+        }"
+      >
+        <BaseIcons name="AngleDown" />
       </div>
-
-      <div v-show="!collapsed[key]" class="wrapper">
-        <BaseJsonViewer v-if="isObject(value) || isArray(value)" :data="value" />
-      </div>
-      <span v-if="isObject(value) && !collapsed[key]" class="ml-4">}</span>
-      <span v-if="isArray(value) && !collapsed[key]" class="ml-4">]</span>
+      <span v-if="!isChildren && isArray(data)">[</span>
+      <span v-if="!isChildren && isObject(data)">{</span>
+      <span v-if="!collapsedRoot && !isChildren && isArray(data)">...]</span>
+      <span v-if="!collapsedRoot && !isChildren && isObject(data)">...}</span>
     </div>
+
+    <div
+      v-if="collapsedRoot"
+      v-for="(value, key) in data"
+      :key="key"
+      class="w-fit ml-4"
+      :class="{
+        wrapper: !isChildren
+      }"
+    >
+      <div
+        :class="{
+          'ml-4': !isChildren
+        }"
+      >
+        <div class="flex">
+          <div
+            v-if="isArray(value) || isObject(value)"
+            @click="toggle(key)"
+            class="arrow-icon"
+            :class="{
+              active: collapsed[key]
+            }"
+          >
+            <BaseIcons name="AngleDown" />
+          </div>
+          <span v-if="!isArray(data)" @click="toggle(key)" class="cursor-pointer">{{ key }}:</span>
+          <span v-if="isObject(value)">
+            <span v-if="collapsed[key]">{...}</span>
+            <span v-else>{</span>
+          </span>
+          <span v-else-if="isArray(value)">
+            <span v-if="collapsed[key]">[...]</span>
+            <span v-else>[</span>
+          </span>
+          <span v-else>{{ value }}</span>
+        </div>
+
+        <div v-show="!collapsed[key]" class="wrapper">
+          <BaseJsonViewer v-if="isObject(value) || isArray(value)" :data="value" isChildren />
+        </div>
+        <span v-if="isObject(value) && !collapsed[key]" class="ml-4">}</span>
+        <span v-if="isArray(value) && !collapsed[key]" class="ml-4">]</span>
+      </div>
+    </div>
+
+    <span v-if="collapsedRoot && !isChildren && isArray(data)" class="ml-4">]</span>
+    <span v-if="collapsedRoot && !isChildren && isObject(data)" class="ml-4">}</span>
   </div>
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 
 interface JsonTreeProps {
   data: Record<string, any> | Array<any>
+  isChildren?: boolean
 }
 
 defineProps<JsonTreeProps>()
 
 const collapsed = reactive<Record<string, boolean>>({})
-
+const collapsedRoot = ref(true)
+const toggleRoot = () => {
+  collapsedRoot.value = !collapsedRoot.value
+}
 const toggle = (key: string) => {
   collapsed[key] = !collapsed[key]
 }
@@ -66,5 +109,8 @@ const isArray = (value: any): boolean => {
   &.active {
     transform: rotate(-90deg);
   }
+}
+.ml-16 {
+  margin-left: 20px;
 }
 </style>
