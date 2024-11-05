00570e7fb5694160c58a974cf0f2fd2bc14cff86
5708646fe8691f1cf581b133aa0b806ac8de5396 <5708646fe8691f1cf581b133aa0b806ac8de5396>
2024-11-05 21:58:06 +0700

    feat: add listDriftOrders, listFillableDriftJITOrders, streamDriftOrders

--- packages/client/indexer/grpc/IndexerGrpcExplorerQuery.ts
+++ packages/client/indexer/grpc/IndexerGrpcExplorerQuery.ts
@@ -116,4 +116,33 @@ export class IndexerGrpcExplorerQuery extends BaseIndexerGrpc {
     const stream = this.client.StreamStrategies(request)
     return stream.subscribe(callback, onEndCallback, onStatusCallback)
   }
+
+  async listDriftOrders(market_name?: string): Promise<explorerQuery.ListDriftOrdersResponse> {
+    const response: explorerQuery.ListDriftOrdersResponse = await this.retry(() =>
+      this.client.ListDriftOrders({
+        market_name
+      })
+    )
+    return response
+  }
+
+  async listFillableDriftJITOrders(
+    params: explorerQuery.ListFillableDriftJITOrdersRequest
+  ): Promise<explorerQuery.ListFillableDriftJITOrdersResponse> {
+    console.log('params', params)
+    const response: explorerQuery.ListFillableDriftJITOrdersResponse = await this.retry(() =>
+      this.client.ListFillableDriftJITOrders(params)
+    )
+    return response
+  }
+
+  async streamDriftOrders(
+    request: Partial<explorerQuery.StreamDriftOrdersRequest>,
+    callback: (value: explorerQuery.StreamDriftOrdersResponse) => void,
+    onEndCallback?: (err: any) => void,
+    onStatusCallback?: () => void
+  ): Promise<Subscription> {
+    const stream = this.client.StreamDriftOrders(request)
+    return stream.subscribe(callback, onEndCallback, onStatusCallback)
+  }
 }
