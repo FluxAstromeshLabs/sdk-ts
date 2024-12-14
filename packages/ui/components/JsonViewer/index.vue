af5ee3fcc9adf64bfaa52aea2ecb544466658062
5708646fe8691f1cf581b133aa0b806ac8de5396 <5708646fe8691f1cf581b133aa0b806ac8de5396>
2024-12-14 20:08:22 +0700

    feat: Add "Show More" button to incrementally render parts of a large JSON array

--- packages/ui/components/JsonViewer/index.vue
+++ packages/ui/components/JsonViewer/index.vue
@@ -62,6 +62,12 @@
         <div v-show="!collapsed[key]" class="wrapper">
           <BaseJsonViewer v-if="isObject(value) || isArray(value)" :data="value" isChildren />
         </div>
+        <span
+          v-if="isArray(data) && Number(key) === Number(data.length - 1) && showButtonShowMoreArray"
+          @click="showMore"
+          class="text-blue-300 text-sm cursor-pointer"
+          >Show more</span
+        >
         <span v-if="isObject(value) && !collapsed[key]" class="space">}</span>
         <span v-if="isArray(value) && !collapsed[key]" class="space">]</span>
       </div>
@@ -73,15 +79,16 @@
 </template>
 
 <script setup lang="ts">
-import { computed, reactive, ref } from 'vue'
+import { computed, onMounted, reactive, ref } from 'vue'
 
 interface JsonTreeProps {
   data: Record<string, any> | Array<any>
   isChildren?: boolean
 }
-
+const ITEM_INCREMENT = 100
 const props = defineProps<JsonTreeProps>()
-
+const maxArrayItem = ref(ITEM_INCREMENT)
+const showButtonShowMoreArray = ref(false)
 const collapsed = reactive<Record<string, boolean>>({})
 const collapsedRoot = ref(true)
 const toggleRoot = () => {
@@ -98,14 +105,19 @@ const isObject = (value: any): boolean => {
 const isArray = (value: any): boolean => {
   return Array.isArray(value)
 }
+const showMore = () => {
+  maxArrayItem.value = Math.min(props.data.length, maxArrayItem.value + ITEM_INCREMENT)
+}
+
 const data = computed<Record<string, any> | Array<any>>(() => {
-  try {
-    let t = JSON.stringify(props.data)
-    t = t.replace(/\\"/g, '"')
-    return JSON.parse(t)
-  } catch (e) {
-    console.log(e)
+  if (isArray(props.data)) {
+    const min = Math.min(props.data.length, maxArrayItem.value)
+    if (min < props.data.length) {
+      showButtonShowMoreArray.value = true
+    }
+    return props.data.slice(0, min)
   }
+  return props.data
 })
 </script>
 
