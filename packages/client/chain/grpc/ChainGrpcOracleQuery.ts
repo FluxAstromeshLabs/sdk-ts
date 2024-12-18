8ee8e64bf433bb243558e4af0a1893491199a76f
5708646fe8691f1cf581b133aa0b806ac8de5396 <5708646fe8691f1cf581b133aa0b806ac8de5396>
2024-12-18 20:25:50 +0700

    feat: add ChainGrpcOracleQuery

---
+++ packages/client/chain/grpc/ChainGrpcOracleQuery.ts
@@ -0,0 +1,19 @@
+import BaseGrpc from '../../BaseGrpc'
+import * as oracle from '../../../../chain/flux/oracle/v1beta1/query'
+export class ChainGrpcOracleQuery extends BaseGrpc {
+  protected client: oracle.QueryClientImpl
+  constructor(endpoint: string) {
+    super(endpoint)
+    this.client = new oracle.QueryClientImpl(this.getGrpcWebImpl(endpoint))
+  }
+  async getPrices(symbols?: string): Promise<oracle.QuerySimpleEntryResponse> {
+    try {
+      let request = oracle.QuerySimpleEntry.create({ symbols: symbols })
+      let response = await this.retry(() => this.client.SimpleEntry(request))
+      return response as oracle.QuerySimpleEntryResponse
+    } catch (e) {
+      console.log(e)
+      throw e
+    }
+  }
+}
