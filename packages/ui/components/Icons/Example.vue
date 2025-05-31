462062b3b0154b0b3b3b5cdf1fc4f0cc21352f6b
chris9962 <chris9962>
2025-05-31 11:02:21 +0700

    feat: add example icons, update icons list, add question icon

---
+++ packages/ui/components/Icons/Example.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="grid grid-cols-4 gap-4">
+    <div v-for="icon in icons.icons" :key="icon">
+      <BaseIcons :name="icon" class="w-10 h-10" />
+      <span>{{ icon }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import icons from '../../icons.json'
+import BaseIcons from './index.vue'
+</script>
