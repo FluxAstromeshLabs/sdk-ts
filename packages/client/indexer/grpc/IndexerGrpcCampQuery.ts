5fd2439b926c5ca84be414ea0e75314c5fe0fd52
chris9962 <chris9962>
2025-06-04 09:17:26 +0700

    feat: add getMissions, checkinDailyLogin func

--- packages/client/indexer/grpc/IndexerGrpcCampQuery.ts
+++ packages/client/indexer/grpc/IndexerGrpcCampQuery.ts
@@ -9,7 +9,7 @@ export class IndexerGrpcCampQuery extends BaseIndexerGrpc {
   }
 
   async listProjects(
-    request: Partial<campclashQuery.ListProjectsRequest>
+    request: Partial<campclashQuery.ListProjectsRequest>,
   ): Promise<campclashQuery.ListProjectsResponse> {
     let response = await this.retry(() => this.client.ListProjects(request))
     return response as campclashQuery.ListProjectsResponse
@@ -19,14 +19,14 @@ export class IndexerGrpcCampQuery extends BaseIndexerGrpc {
     request: Partial<campclashQuery.StreamProjectRequest>,
     callback: (value: campclashQuery.StreamProjectResponse) => void,
     onEndCallback?: (err: any) => void,
-    onStatusCallback?: () => void
+    onStatusCallback?: () => void,
   ): Promise<Subscription> {
     const stream = this.client.StreamProject(request)
     return stream.subscribe(callback, onEndCallback, onStatusCallback)
   }
 
   async listBalances(
-    request: Partial<campclashQuery.ListBalancesRequest>
+    request: Partial<campclashQuery.ListBalancesRequest>,
   ): Promise<campclashQuery.ListBalancesResponse> {
     let response = await this.retry(() => this.client.ListBalances(request))
     return response as campclashQuery.ListBalancesResponse
@@ -36,14 +36,14 @@ export class IndexerGrpcCampQuery extends BaseIndexerGrpc {
     request: Partial<campclashQuery.StreamBalancesRequest>,
     callback: (value: campclashQuery.StreamBalancesResponse) => void,
     onEndCallback?: (err: any) => void,
-    onStatusCallback?: () => void
+    onStatusCallback?: () => void,
   ): Promise<Subscription> {
     const stream = this.client.StreamBalances(request)
     return stream.subscribe(callback, onEndCallback, onStatusCallback)
   }
 
   async listTrades(
-    request: Partial<campclashQuery.ListTradesRequest>
+    request: Partial<campclashQuery.ListTradesRequest>,
   ): Promise<campclashQuery.ListTradesResponse> {
     let response = await this.retry(() => this.client.ListTrades(request))
     return response as campclashQuery.ListTradesResponse
@@ -53,19 +53,19 @@ export class IndexerGrpcCampQuery extends BaseIndexerGrpc {
     request: Partial<campclashQuery.StreamTradesRequest>,
     callback: (value: campclashQuery.StreamTradesResponse) => void,
     onEndCallback?: (err: any) => void,
-    onStatusCallback?: () => void
+    onStatusCallback?: () => void,
   ): Promise<Subscription> {
     const stream = this.client.StreamTrades(request)
     return stream.subscribe(callback, onEndCallback, onStatusCallback)
   }
   async listComments(
-    request: Partial<campclashQuery.ListCommentsRequest>
+    request: Partial<campclashQuery.ListCommentsRequest>,
   ): Promise<campclashQuery.ListCommentsResponse> {
     let response = await this.retry(() => this.client.ListComments(request))
     return response as campclashQuery.ListCommentsResponse
   }
   async postComment(
-    request: Partial<campclashQuery.PostCommentRequest>
+    request: Partial<campclashQuery.PostCommentRequest>,
   ): Promise<campclashQuery.PostCommentResponse> {
     let response = await this.retry(() => this.client.PostComment(request))
     return response as campclashQuery.PostCommentResponse
@@ -74,19 +74,19 @@ export class IndexerGrpcCampQuery extends BaseIndexerGrpc {
     request: Partial<campclashQuery.StreamCommentsRequest>,
     callback: (value: campclashQuery.StreamCommentsResponse) => void,
     onEndCallback?: (err: any) => void,
-    onStatusCallback?: () => void
+    onStatusCallback?: () => void,
   ): Promise<Subscription> {
     const stream = this.client.StreamComments(request)
     return stream.subscribe(callback, onEndCallback, onStatusCallback)
   }
   async getLeaderboard(
-    request: Partial<campclashQuery.GetLeaderboardRequest>
+    request: Partial<campclashQuery.GetLeaderboardRequest>,
   ): Promise<campclashQuery.GetLeaderboardResponse> {
     let response = await this.retry(() => this.client.GetLeaderboard(request))
     return response as campclashQuery.GetLeaderboardResponse
   }
   async listChallenge(
-    request: Partial<campclashQuery.ListChallengeRequest>
+    request: Partial<campclashQuery.ListChallengeRequest>,
   ): Promise<campclashQuery.ListChallengeResponse> {
     let response = await this.retry(() => this.client.ListChallenge(request))
     return response as campclashQuery.ListChallengeResponse
@@ -95,13 +95,13 @@ export class IndexerGrpcCampQuery extends BaseIndexerGrpc {
     request: Partial<campclashQuery.StreamChallengeRequest>,
     callback: (value: campclashQuery.StreamChallengeResponse) => void,
     onEndCallback?: (err: any) => void,
-    onStatusCallback?: () => void
+    onStatusCallback?: () => void,
   ): Promise<Subscription> {
     const stream = this.client.StreamChallenge(request)
     return stream.subscribe(callback, onEndCallback, onStatusCallback)
   }
   async listChallengeClaimable(
-    request: Partial<campclashQuery.ListChallengeClaimableRequest>
+    request: Partial<campclashQuery.ListChallengeClaimableRequest>,
   ): Promise<campclashQuery.ListChallengeClaimableResponse> {
     let response = await this.retry(() => this.client.ListChallengeClaimable(request))
     return response as campclashQuery.ListChallengeClaimableResponse
@@ -110,7 +110,7 @@ export class IndexerGrpcCampQuery extends BaseIndexerGrpc {
     request: Partial<campclashQuery.StreamChallengeClaimableRequest>,
     callback: (value: campclashQuery.StreamChallengeClaimableResponse) => void,
     onEndCallback?: (err: any) => void,
-    onStatusCallback?: () => void
+    onStatusCallback?: () => void,
   ): Promise<Subscription> {
     const stream = this.client.StreamChallengeClaimable(request)
     return stream.subscribe(callback, onEndCallback, onStatusCallback)
@@ -119,19 +119,19 @@ export class IndexerGrpcCampQuery extends BaseIndexerGrpc {
     request: Partial<campclashQuery.StreamChallengeVoteRequest>,
     callback: (value: campclashQuery.StreamChallengeVoteResponse) => void,
     onEndCallback?: (err: any) => void,
-    onStatusCallback?: () => void
+    onStatusCallback?: () => void,
   ): Promise<Subscription> {
     const stream = this.client.StreamChallengeVote(request)
     return stream.subscribe(callback, onEndCallback, onStatusCallback)
   }
   async getUserChallenges(
-    request: Partial<campclashQuery.GetUserChallengesRequest>
+    request: Partial<campclashQuery.GetUserChallengesRequest>,
   ): Promise<campclashQuery.GetUserChallengesResponse> {
     let response = await this.retry(() => this.client.GetUserChallenges(request))
     return response as campclashQuery.GetUserChallengesResponse
   }
   async getLogoPresignedURL(
-    request: Partial<campclashQuery.GetLogoPresignedURLRequest>
+    request: Partial<campclashQuery.GetLogoPresignedURLRequest>,
   ): Promise<campclashQuery.GetLogoPresignedURLResponse> {
     let response = await this.retry(() => this.client.GetLogoPresignedURL(request))
     return response as campclashQuery.GetLogoPresignedURLResponse
@@ -140,13 +140,13 @@ export class IndexerGrpcCampQuery extends BaseIndexerGrpc {
     request: Partial<campclashQuery.PushUserActivityRequest>,
     callback: (value: campclashQuery.PushUserActivityResponse) => void,
     onEndCallback?: (err: any) => void,
-    onStatusCallback?: () => void
+    onStatusCallback?: () => void,
   ): Promise<Subscription> {
     const stream = this.client.PushUserActivity(request)
     return stream.subscribe(callback, onEndCallback, onStatusCallback)
   }
   async listUserPoints(
-    request: Partial<campclashQuery.ListUserPointsRequest>
+    request: Partial<campclashQuery.ListUserPointsRequest>,
   ): Promise<campclashQuery.ListUserPointsResponse> {
     let response = await this.retry(() => this.client.ListUserPoints(request))
     return response as campclashQuery.ListUserPointsResponse
@@ -155,9 +155,21 @@ export class IndexerGrpcCampQuery extends BaseIndexerGrpc {
     request: Partial<campclashQuery.StreamUserPointsRequest>,
     callback: (value: campclashQuery.StreamUserPointsResponse) => void,
     onEndCallback?: (err: any) => void,
-    onStatusCallback?: () => void
+    onStatusCallback?: () => void,
   ): Promise<Subscription> {
     const stream = this.client.StreamUserPoints(request)
     return stream.subscribe(callback, onEndCallback, onStatusCallback)
+  }
+  async getMissions(
+    request: Partial<campclashQuery.GetMissionsRequest>,
+  ): Promise<campclashQuery.GetMissionsResponse> {
+    let response = await this.retry(() => this.client.GetMissions(request))
+    return response as campclashQuery.GetMissionsResponse
+  }
+  async checkinDailyLogin(
+    request: Partial<campclashQuery.CheckinDailyLoginRequest>,
+  ): Promise<campclashQuery.CheckinDailyLoginResponse> {
+    let response = await this.retry(() => this.client.CheckinDailyLogin(request))
+    return response as campclashQuery.CheckinDailyLoginResponse
   }
 }
